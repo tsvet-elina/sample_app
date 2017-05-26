f5de5e43a3d728eb7fe546b708024ed8c5376979
tsvet-elina <panayotova.tsvetelina@gmail.com>
2017-05-26 17:39:03 +0300

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,8 +6,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
     @other_user = users(:archer)
 
-
   end
+
+    test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
 
   test "should get new" do
     get users_new_url
@@ -27,7 +32,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-test "should redirect edit when logged in as wrong user" do
+  test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
     assert flash.empty?
@@ -36,10 +41,24 @@ test "should redirect edit when logged in as wrong user" do
 
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
+    patch user_path(@user), params: {User: { name: @user.name,
+                                             email: @user.email } }
     assert flash.empty?
     assert_redirected_to root_url
   end
   
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
